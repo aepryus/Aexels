ec71edf16d08cc778bd7d7b97e62fca214cf3fc5
Joe Charlier <joe@aepryus.com>
2017-01-02 19:27:18 -0800

    resouces

---
+++ Aexels/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Aexels
+//
+//  Created by Joe Charlier on 1/2/17.
+//  Copyright © 2017 Aepryus Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
