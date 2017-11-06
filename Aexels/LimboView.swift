51ce12ede7b509aba90da24b57256ab42ab5addf
Joe Charlier <joe@aepryus.com>
2017-11-06 14:09:37 +0900

    fabric update; 1024 icon; misc Oovium public's added

--- Aexels/LimboView.swift
+++ Aexels/LimboView.swift
@@ -48,15 +48,6 @@ class LimboView: UIView {
 	}
 
 	var limboPath = LimboPath()
-//	var limboPath: LimboPath {
-//		set {
-//			_limboPath = newValue
-//			setNeedsDisplay()
-//			self.layer.shadowPath = limboPath.shadowPath
-//			applyMask(_limboPath.maskPath)
-//		}
-//		get {return _limboPath}
-//	}
 	
 	init() {
 		super.init(frame: CGRect.zero)
@@ -77,34 +68,6 @@ class LimboView: UIView {
 	}
 	required init? (coder aDecoder: NSCoder) {fatalError()}
 	
-	private static func buildTopRightCutoutPath (x: [CGFloat], y: [CGFloat], radius: CGFloat) -> CGPath {
-		let path = CGMutablePath()
-		
-		path.move(to: CGPoint(x: x[0], y: y[1]))
-		path.addArc(tangent1End: CGPoint(x: x[0], y: y[0]), tangent2End: CGPoint(x: x[1], y: y[0]), radius: radius)
-		path.addArc(tangent1End: CGPoint(x: x[3], y: y[0]), tangent2End: CGPoint(x: x[3], y: y[4]), radius: radius)
-		path.addArc(tangent1End: CGPoint(x: x[3], y: y[3]), tangent2End: CGPoint(x: x[4], y: y[3]), radius: radius)
-		path.addArc(tangent1End: CGPoint(x: x[2], y: y[3]), tangent2End: CGPoint(x: x[2], y: y[1]), radius: radius)
-		path.addArc(tangent1End: CGPoint(x: x[2], y: y[2]), tangent2End: CGPoint(x: x[1], y: y[2]), radius: radius)
-		path.addArc(tangent1End: CGPoint(x: x[0], y: y[2]), tangent2End: CGPoint(x: x[0], y: y[1]), radius: radius)
-		path.closeSubpath()
-		
-		return path
-	}
-	private static func buildBottomRightCutoutPath (x: [CGFloat], y: [CGFloat], radius: CGFloat) -> CGPath {
-		let path = CGMutablePath()
-		
-		path.move(to: CGPoint(x: x[0], y: y[1]))
-		path.addArc(tangent1End: CGPoint(x: x[0], y: y[0]), tangent2End: CGPoint(x: x[1], y: y[0]), radius: radius)
-		path.addArc(tangent1End: CGPoint(x: x[2], y: y[0]), tangent2End: CGPoint(x: x[2], y: y[1]), radius: radius)
-		path.addArc(tangent1End: CGPoint(x: x[2], y: y[3]), tangent2End: CGPoint(x: x[4], y: y[3]), radius: radius)
-		path.addArc(tangent1End: CGPoint(x: x[3], y: y[3]), tangent2End: CGPoint(x: x[3], y: y[4]), radius: radius)
-		path.addArc(tangent1End: CGPoint(x: x[3], y: y[2]), tangent2End: CGPoint(x: x[1], y: y[2]), radius: radius)
-		path.addArc(tangent1End: CGPoint(x: x[0], y: y[2]), tangent2End: CGPoint(x: x[0], y: y[1]), radius: radius)
-		path.closeSubpath()
-		
-		return path
-	}
 	private func buildPath (p: CGFloat, q: CGFloat) -> CGPath {
 		let path = CGMutablePath()
 		
@@ -117,7 +80,7 @@ class LimboView: UIView {
 		
 		path.move(to: CGPoint(x: x1, y: y2))
 		
-		if let cutout = cutouts[Position.topLeft] {
+		if let cutout = cutouts[.topLeft] {
 			let xe = x1 + cutout.width + q
 			let xc = (x1 + xe) / 2
 			let ye = y1 + cutout.height + q
@@ -129,7 +92,7 @@ class LimboView: UIView {
 			path.addArc(tangent1End: CGPoint(x: x1, y: y1), tangent2End: CGPoint(x: x2, y: y1), radius: radius)
 		}
 		
-		if let cutout = cutouts[Position.topRight] {
+		if let cutout = cutouts[.topRight] {
 			let xe = x3 - cutout.width - q
 			let xc = (xe + x3) / 2
 			let ye = y1 + cutout.height + q
@@ -141,7 +104,7 @@ class LimboView: UIView {
 			path.addArc(tangent1End: CGPoint(x: x3, y: y1), tangent2End: CGPoint(x: x3, y: y2), radius: radius)
 		}
 		
-		if let cutout = cutouts[Position.bottomRight] {
+		if let cutout = cutouts[.bottomRight] {
 			let xe = x3 - cutout.width - q
 			let xc = (xe + x3) / 2
 			let ye = y3 - cutout.height - q
@@ -153,7 +116,7 @@ class LimboView: UIView {
 			path.addArc(tangent1End: CGPoint(x: x3, y: y3), tangent2End: CGPoint(x: x2, y: y3), radius: radius)
 		}
 		
-		if let cutout = cutouts[Position.bottomLeft] {
+		if let cutout = cutouts[.bottomLeft] {
 			let xe = x1 + cutout.width + q
 			let xc = (x1 + xe) / 2
 			let ye = y3 - cutout.height - q
@@ -171,88 +134,10 @@ class LimboView: UIView {
 	}
 	
 	func renderPaths() {
-		
 		limboPath.strokePath = buildPath(p: a, q: 0)
 		limboPath.maskPath = buildPath(p: a, q: a-b)
 		limboPath.shadowPath = buildPath(p: b, q: 0)
 		
-//		// Stroke
-//		var x1 = a
-//		var x3 = width - a
-//		var x2 = (x1 + x3) / 2
-//		var x4: CGFloat = 0
-//		var x5: CGFloat = 0
-//		
-//		var y1 = a
-//		var y3 = height - a
-//		var y2 = (y1 + y3) / 2
-//		var y4: CGFloat = 0
-//		var y5: CGFloat = 0
-//		
-//		if let cutout = cutout {
-//			if cutout.point.isRight() {
-//				x4 = x3 - cutout.width
-//				x5 = (x3 + x4) / 2
-//			}
-//			if cutout.point.isTop() {
-//				y4 = y1 + cutout.height
-//				y5 = (y1 + y4) / 2
-//				limboPath.strokePath = LimboView.buildTopRightCutoutPath(x: [x1,x2,x3,x4,x5], y: [y1,y2,y3,y4,y5], radius: radius)
-//			} else {
-//				y4 = y3 - cutout.height
-//				y5 = (y3 + y4) / 2
-//				limboPath.strokePath = LimboView.buildBottomRightCutoutPath(x: [x1,x2,x3,x4,x5], y: [y1,y2,y3,y4,y5], radius: radius)
-//			}
-//		} else {
-//			limboPath.strokePath = CGPath(roundedRect: CGRect(x: x1, y: y1, width: x3-x1, height: y3-y1), cornerWidth: radius, cornerHeight: radius, transform: nil)
-//		}
-		
-//		// Mask
-//		if let cutout = cutout {
-//			if cutout.point.isRight() {
-//				x4 = x3 - cutout.width - a + b
-//				x5 = (x3 + x4) / 2
-//			}
-//			if cutout.point.isTop() {
-//				y4 = y1 + cutout.height + a - b
-//				y5 = (y1 + y3) / 2
-//				limboPath.maskPath = LimboView.buildTopRightCutoutPath(x: [x1,x2,x3,x4,x5], y: [y1,y2,y3,y4,y5], radius: radius)
-//			} else {
-//				y4 = y3 - cutout.height - a + b
-//				y5 = (y3 + y4) / 2
-//				limboPath.maskPath = LimboView.buildBottomRightCutoutPath(x: [x1,x2,x3,x4,x5], y: [y1,y2,y3,y4,y5], radius: radius)
-//			}
-//		} else {
-//			limboPath.maskPath = limboPath.strokePath
-//		}
-		
-//		// Shadow
-//		x1 = b
-//		x3 = width - b
-//		x2 = (x1 + x3) / 2
-//		
-//		y1 = b
-//		y3 = height - b
-//		y2 = (y1 + y3) / 2
-//		
-//		if let cutout = cutout {
-//			if cutout.point.isRight() {
-//				x4 = x3 - cutout.width
-//				x5 = (x3 + x4) / 2
-//			}
-//			if cutout.point.isTop() {
-//				y4 = y1 + cutout.height
-//				y5 = (y1 + y4) / 2
-//				limboPath.shadowPath = LimboView.buildTopRightCutoutPath(x: [x1,x2,x3,x4,x5], y: [y1,y2,y3,y4,y5], radius: radius)
-//			} else {
-//				y4 = y3 - cutout.height
-//				y5 = (y3 + y4) / 2
-//				limboPath.shadowPath = LimboView.buildBottomRightCutoutPath(x: [x1,x2,x3,x4,x5], y: [y1,y2,y3,y4,y5], radius: radius)
-//			}
-//		} else {
-//			limboPath.shadowPath = CGPath(roundedRect: CGRect(x: x1, y: y1, width: x3-x1, height: y3-y1), cornerWidth: radius, cornerHeight: radius, transform: nil)
-//		}
-
 		applyMask(limboPath.maskPath)
 		self.layer.shadowPath = limboPath.shadowPath
 		setNeedsDisplay()
@@ -262,8 +147,7 @@ class LimboView: UIView {
 	
 // UIView ==========================================================================================
 	override var frame: CGRect {
-		set {
-			super.frame = newValue
+		didSet {
 			guard frame != CGRect.zero else {return}
 			limboPath.strokePath = CGPath(roundedRect: bounds.insetBy(dx: 6, dy: 6), cornerWidth: 10, cornerHeight: 10, transform: nil)
 			limboPath.shadowPath = CGPath(roundedRect: bounds.insetBy(dx: 2, dy: 2), cornerWidth: 10, cornerHeight: 10, transform: nil)
@@ -271,9 +155,6 @@ class LimboView: UIView {
 			self.layer.shadowPath = limboPath.shadowPath
 			
 			renderPaths()
-		}
-		get {
-			return super.frame
 		}
 	}
 	override func draw(_ rect: CGRect) {
