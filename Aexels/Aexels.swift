88acd6a5d12c546b60cda99434b4ed5729f16c60
Joe Charlier <joe@aepryus.com>
2019-05-11 01:49:44 +0900

    changes from Sphyrnidae

--- Aexels/Aexels.swift
+++ Aexels/Aexels.swift
@@ -26,9 +26,7 @@ class Aexels {
 				return UIImage(named: "Back4")!
 			case .dim320x568, .dim375x667, .dim414x736, .dim375x812, .dim414x896:
 				return UIImage(named: "Back6")!
-			case .dim1024x768, .dim1112x834, .dim1366x1024:
-				return UIImage(named: "BackiPad")!
-			case .dim1194x834:
+			case .dim1024x768, .dim1112x834, .dim1194x834, .dim1366x1024:
 				return UIImage(named: "BackiPad")!
 			case .dimOther:
 				return UIImage(named: "Back5")!
--- Aexels/Explorer.swift
+++ Aexels/Explorer.swift
@@ -38,30 +38,34 @@ class Explorer {
 
 	func layout() {
 		switch Screen.this {
+			case .dim320x480:	layout320x480()
 			case .dim320x568:	layout320x568()
 			case .dim375x667:	layout375x667()
+			case .dim375x812:	layout375x812()
 			case .dim414x736:	layout414x736()
+			case .dim414x896:	layout414x896()
 			case .dim1024x768:	layout1024x768()
 			case .dim1112x834:	layout1112x834()
+			case .dim1194x834:	layout1194x834()
 			case .dim1366x1024:	layout1366x1024()
-			default:			layout375x667()
+			case .dimOther:		layout375x667()
 		}
 	}
 	
-	func layout320x568() {
-		layout375x667()
-	}
+	func layout320x480() {layout375x667()}
+
+	func layout320x568() {layout375x667()}
 	func layout375x667() {}
-	func layout414x736() {
-		layout320x568()
-	}
+	func layout414x736() {layout375x667()}
+
+	func layout375x812() {layout375x667()}
+	func layout414x896() {layout375x812()}
+
 	func layout1024x768() {}
-	func layout1112x834() {
-		layout1024x768()
-	}
-	func layout1366x1024() {
-		layout1024x768()
-	}
+	func layout1112x834() {layout1024x768()}
+	func layout1366x1024() {layout1024x768()}
+
+	func layout1194x834() {layout1024x768()}
 
 	func dimLimbos (_ limbos: [Limbo]) {
 		UIView.animate(withDuration: 0.2, animations: { 
