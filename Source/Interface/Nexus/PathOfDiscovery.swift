5ebd5c191519b542345461adfae3deb55899b1f9
Joe Charlier <github@aepryus.com>
2025-03-12 10:52:15 +0900

    Path of Discovery work

--- Source/Interface/Nexus/PathOfDiscovery.swift
+++ Source/Interface/Nexus/PathOfDiscovery.swift
@@ -27,12 +27,12 @@ class PathOfDiscovery {
         
         let discoveryGlyph: ArticleGlyph = ArticleGlyph(article: discovery, radius: 112*s+2*p, x: 200*s, y: 30*s)
         let expansionGlyph: ArticleGlyph = ArticleGlyph(article: expansion, radius: 112*s+2*p, x: 70*s, y: 180*s)
-        let escapingGlyph: ArticleGlyph = ArticleGlyph(article: escaping, radius: 96*s+2*p, x: 200*s, y: 270*s)
-        let covarianceGlyph: ArticleGlyph = ArticleGlyph(article: covariance, radius: 118*s+2*p, x: 230*s, y: 410*s)
-        let reciprocityGlyph: ArticleGlyph = ArticleGlyph(article: reciprocity, radius: 112*s+2*p, x: 70*s, y: 580*s)
-        let eeemceeGlyph: ArticleGlyph = ArticleGlyph(article: eeemcee, radius: 120*s+2*p, x: 150*s, y: 730*s)
-        let cupolaGlyph: ArticleGlyph = ArticleGlyph(article: cupola, radius: 80*s+2*p, x: 70*s, y: 930*s)
-        let squishGlyph: ArticleGlyph = ArticleGlyph(article: squish, radius: 80*s+2*p, x: 270*s, y: 950*s)
+        let escapingGlyph: ArticleGlyph = ArticleGlyph(article: escaping, radius: 102*s+2*p, x: 200*s, y: 270*s)
+        let covarianceGlyph: ArticleGlyph = ArticleGlyph(article: covariance, radius: 124*s+2*p, x: 230*s, y: 410*s)
+        let reciprocityGlyph: ArticleGlyph = ArticleGlyph(article: reciprocity, radius: 128*s+2*p, x: 70*s, y: 580*s)
+        let eeemceeGlyph: ArticleGlyph = ArticleGlyph(article: eeemcee, radius: 132*s+2*p, x: 150*s, y: 730*s)
+        let cupolaGlyph: ArticleGlyph = ArticleGlyph(article: cupola, radius: 92*s+2*p, x: 70*s, y: 930*s)
+        let squishGlyph: ArticleGlyph = ArticleGlyph(article: squish, radius: 88*s+2*p, x: 270*s, y: 950*s)
         
         let insideOutExpGlyph: ExplorerGlyph = ExplorerGlyph(explorer: Aexels.distanceExplorer, radius: 50*s+2*p, x: 380*s, y: 890*s)
         let outsideInExpGlyph: ExplorerGlyph = ExplorerGlyph(explorer: Aexels.gravityExplorer, radius: 50*s+2*p, x: 380*s, y: 1040*s)
